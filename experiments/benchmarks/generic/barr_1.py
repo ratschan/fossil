12393b52af0eabf4659594435c08df8ce6a9757f
Stefan Ratschan <stefan.ratschan@fit.cvut.cz>
2024-11-21 21:42:04 +0100

    import constraints

--- experiments/benchmarks/generic/barr_1.py
+++ experiments/benchmarks/generic/barr_1.py
@@ -11,37 +11,7 @@ from fossil import certificate
 from fossil import main
 from experiments.benchmarks import models
 from fossil.consts import *
-
-def sign_loss_neg(V, Vdot, circle):  
-  return V
-
-def sign_verif_neg(connectives, variables, V, Vdot):
-  return V>=0
-
-def sign_loss_pos(V, Vdot, circle):  
-  return V
-
-def sign_verif_pos(connectives, variables, V, Vdot):
-  return V<=0
-
-def barrier_loss_belt(B_d, Bdot_d, circle):            
-        margin = 0
-        belt_index = torch.nonzero(torch.abs(B_d) <= 0.5)
-
-        if belt_index.nelement() != 0:
-            dB_belt = torch.index_select(Bdot_d, dim=0, index=belt_index[:, 0])          # choose the elements of Bdot_d for which B_d is close to zero
-            loss= (torch.relu(dB_belt + 0 * margin)).mean()
-            percent_belt = (
-                100 * ((dB_belt <= -margin).count_nonzero()).item() / dB_belt.shape[0]
-            )            
-        else:
-            loss= 0
-            percent_belt= 0
-          
-        return loss, percent_belt
-
-def barrier_verif(connectives, variables, V, Vdot):
-        return _And(V == 0, Vdot >= 0)
+from fossil.constraints import negative, positive, barrier
 
 class UnsafeDomain(domains.Set):
     dimension = 2
@@ -77,9 +47,9 @@ def test_lnn(args):
     }
 
     constraints = {
-      "initial": {"loss": sign_loss_neg, "verif": sign_verif_neg},
-      "unsafe": {"loss": sign_loss_pos, "verif": sign_verif_pos},
-      "inductivity": {"loss": barrier_loss_belt, "verif": barrier_verif }
+      "initial": negative,
+      "unsafe": positive,
+      "inductivity": barrier
     }
 
     
